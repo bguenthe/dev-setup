8274009dfae02930989583f48106eb4ffb48e5cd
Hermann Vocke <hvocke@thoughtworks.com>
2017-07-28 14:35:37 +0200

    make curl quiet

--- provisioning/terraform.sh
+++ provisioning/terraform.sh
@@ -5,10 +5,10 @@ TERRAFORM_VERSION="0.9.11"
 echo "Install Terraform"
 
 # download Terraform
-curl https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip > terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+curl -s https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip > terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 
 # download Terraform checksum
-curl https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_SHA256SUMS > terraform_${TERRAFORM_VERSION}_SHA256SUMS_all
+curl -s https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_SHA256SUMS > terraform_${TERRAFORM_VERSION}_SHA256SUMS_all
 
 # remove unneeded checksums
 grep "terraform_${TERRAFORM_VERSION}_linux_amd64.zip" terraform_${TERRAFORM_VERSION}_SHA256SUMS_all > terraform_${TERRAFORM_VERSION}_SHA256SUMS
