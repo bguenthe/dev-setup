a98d6666e7f5c9adb35d5107350e49c0fb2a22c2
Hermann Vocke <hvocke@thoughtworks.com>
2017-07-28 14:16:44 +0200

    change starting directory to /repos

--- provisioning/common.sh
+++ provisioning/common.sh
@@ -3,7 +3,9 @@
 echo "Install Tools"
 sudo apt-get update
 sudo apt-get install -y unzip jq python-pip git-core ntp
-echo "cd /infrastructure/" >> /home/ubuntu/.bashrc
+
+# Automatically go to /repos directory on login
+echo "cd /repos/" >> /home/ubuntu/.bashrc
 
 # Start SSH agent via bashrc if it's not running yet
 echo "
