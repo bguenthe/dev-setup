5cb9881d74c640f36217bf297300f4855e08335e
apahlke <andreas.pahlke@otto.de>
2017-08-09 12:34:45 +0200

    kopieren der Schlüssel

---
+++ init.bat
@@ -0,0 +1,12 @@
+::echo off
+
+cd provisioning
+if not exist .secrets (mkdir .secrets)
+cd ..
+
+copy g:\.ssh\id_rsa provisioning\.secrets
+copy g:\.ssh\id_rsa.pub provisioning\.secrets
+
+copy  %UserProfile%\.aws\credentials provisioning\.secrets
+
+vagrant up
