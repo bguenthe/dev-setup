24a0b9c39adddd61b384cd4dfdae14c0e1c6d06e
bguenthe <bernd.stuebe@gmail.com>
2017-12-10 18:14:39 +0100

    - npm und gradle added - allow symlinks under windows

--- provisioning/npmnode.sh
+++ provisioning/npmnode.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-apt-get install -y nodejs
+sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
-npm install -g @angular/cli
+sudo npm install -g @angular/cli
